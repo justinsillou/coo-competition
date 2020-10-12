c725bb447b9987ecf7a9de20ba6ab424c95cd8cc
sillou <justin.sillou.etu@univ-lille.fr>
2020-10-12 12:01:16 +0200

    ajout de la documentation generee

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"competition","l":"Competition"},{"p":"competitor","l":"Competitor"},{"p":"competition","l":"League"},{"p":"competition","l":"Main"},{"p":"util","l":"MapUtil"},{"p":"match","l":"Match"},{"p":"competition","l":"Tournament"}]
